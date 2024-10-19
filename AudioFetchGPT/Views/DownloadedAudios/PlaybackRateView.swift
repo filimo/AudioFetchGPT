a2bbad9ae074911c7f07f815b0554f6306082516
Viktor Kushnerov <filimo@Viktors-MacBook-Pro.local>
2024-10-19 10:36:03 +0300

    Updates playback speed precision in PlaybackRateView
    - Updated playback speed display to two decimal places in `PlaybackRateView.swift`.
    - Adjusted the playback speed increment/decrement steps from 0.1 to 0.05 for finer control.
    - This change allows users to more accurately adjust the audio playback speed.

--- AudioFetchGPT/Views/DownloadedAudios/PlaybackRateView.swift
+++ AudioFetchGPT/Views/DownloadedAudios/PlaybackRateView.swift
@@ -10,12 +10,12 @@ struct PlaybackRateView: View {
     
     var body: some View {
         HStack {
-            Text("Speed: \(audioManager.playbackRate, specifier: "%.1f")x") // Display current playback rate
+            Text("Speed: \(audioManager.playbackRate, specifier: "%.2f")x") // Display current playback rate
             
             // Кнопка для уменьшения скорости воспроизведения
             Button(action: {
                 if audioManager.playbackRate > 0.5 {
-                    audioManager.setPlaybackRate(audioManager.playbackRate - 0.1)
+                    audioManager.setPlaybackRate(audioManager.playbackRate - 0.05)
                 }
             }) {
                 Image(systemName: "minus.circle")
@@ -28,12 +28,12 @@ struct PlaybackRateView: View {
                 set: { newValue in
                     audioManager.setPlaybackRate(newValue)
                 }
-            ), in: 0.5 ... 2.0, step: 0.1) // Slider for selecting playback rate
+            ), in: 0.5 ... 2.0, step: 0.05) // Slider for selecting playback rate
             
             // Кнопка для увеличения скорости воспроизведения
             Button(action: {
                 if audioManager.playbackRate < 2.0 {
-                    audioManager.setPlaybackRate(audioManager.playbackRate + 0.1)
+                    audioManager.setPlaybackRate(audioManager.playbackRate + 0.05)
                 }
             }) {
                 Image(systemName: "plus.circle")
